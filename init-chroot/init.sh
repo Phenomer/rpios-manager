f68968146be5ac274cdc2346f9df867bb1d0d400
Akito Miura <phenomer@g.hachune.net>
2022-06-11 02:51:21 +0900

    イメージのベース環境展開スクリプトを追加

---
+++ init-chroot/init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+set -x
+
+ROOT_DIR='/rpios'
+
+ARCHIVE_DATE=`curl -q -o- https://ftp.jaist.ac.jp/pub/raspberrypi/raspios_arm64/archive/ |
+  grep -E -o 'href=\"\./[0-9:-]*/\"' |
+  sed -e 's|href="./||' -e 's|/"||' |
+  tail -n 1`
+echo ${ARCHIVE_DATE}
+
+umask 022
+if [ ! -e 'boot.tar.xz' ]; then
+  curl -o boot.tar.xz "https://ftp.jaist.ac.jp/pub/raspberrypi/raspios_arm64/archive/${ARCHIVE_DATE}/boot.tar.xz"
+fi
+
+if [ ! -e 'root.tar.xz' ]; then
+  curl -o root.tar.xz "https://ftp.jaist.ac.jp/pub/raspberrypi/raspios_arm64/archive/${ARCHIVE_DATE}/root.tar.xz"
+fi
+
+tar xpf root.tar.xz -C "${ROOT_DIR}"
+tar xpf boot.tar.xz -C "${ROOT_DIR}/boot"
